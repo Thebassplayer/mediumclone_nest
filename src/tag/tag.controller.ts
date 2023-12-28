a6e00b79895f85c1afaa3fa5cbce1b8b9511e0b8
Roy Lopez <roylopezdev@gmail.com>
2023-12-28 17:14:43 -0300

    Add absolute path

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { TagService } from '@app/tag/tag.service';
 
 @Controller('tags')
 export class TagController {
+  constructor(private readonly tagService: TagService) {}
   @Get()
-  findAll() {
-    return ['dogs'];
+  findAll(): string[] {
+    return this.tagService.findAll();
   }
 }
